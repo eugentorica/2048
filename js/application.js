9952f5006583e88a9183a44cb62688c3b2d37881
Eugeniu Torica <eugen.torica@gmail.com>
2015-07-24 17:32:53 +0300

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,4 +1,21 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  var game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  window.game = game;
+  
+  window.moveUp = function(){
+	game.inputManager.emit("move", 0);
+  }
+  
+  window.moveDown = function(){
+	game.inputManager.emit("move", 2);
+  }
+   
+  window.moveRight = function(){
+	game.inputManager.emit("move", 1);
+  }
+  
+  window.moveLeft = function(){
+	game.inputManager.emit("move", 3);
+  }
 });
